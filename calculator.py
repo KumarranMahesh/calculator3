4debc445e0a55cbfa6422bc2d96149a97cf0b6d0
KumarranMahesh <69680214+KumarranMahesh@users.noreply.github.com>
2020-10-17 11:26:50 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,11 @@
 # This calculator only adds and subtracts
+a = int(input("Enter first number : "))
+opr = int(input("Enter operator (+/-) : "))
+b = int(input("Enter second number : "))
+
+if opr == "+":
+  print(f"The sum is : {a + b}")
+elif opr == "-":
+  print(f"The sum is : {a - b}")  
+else:
+  print("Invalid Input")
